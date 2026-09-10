972804bc08ddfe562cbf22e18e5fd551651dde41
Devendra Solanki <mee.dev.2026@email.com>
2026-09-10 10:29:08 +0530

    Setup MOQ

--- SparkyNUnitTests/BankAccountNUnitTests.cs
+++ SparkyNUnitTests/BankAccountNUnitTests.cs
@@ -1,20 +1,33 @@
 using System;
 using NUnit.Framework;
+using Moq;
 namespace Sparky;
 
 [TestFixture]
 public class BankAccountNUnitTests
 {
-    private BankAccount bankAccount;
+    private BankAccount account;
     [SetUp]
     public void SetUp()
     {
-        bankAccount = new(new LogBookFake());
+        
     }
 
     [Test]
-    public void BankDeposit_Added100_ReturnsTrue()
+    public void BankDepositFakeLogger_Added100_ReturnsTrue()
     {
+        BankAccount bankAccount = new(new LogBookFake());
+        var result = bankAccount.Deposit(100);
+        Assert.IsTrue(result);
+        Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
+    }
+
+    [Test]
+    public void BankDepositMoq_Added100_ReturnsTrue()
+    {
+        var logMoq = new Mock<ILogBook>();
+        logMoq.Setup(x=> x.Message(""));
+        BankAccount bankAccount = new(logMoq.Object);
         var result = bankAccount.Deposit(100);
         Assert.IsTrue(result);
         Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
